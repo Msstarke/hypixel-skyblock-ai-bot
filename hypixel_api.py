0e56873139292c6a20ec262540d6d0074d5423b5
Msstarke <msstarke@users.noreply.github.com>
2026-03-19 17:00:37 +1100

    Auto: update hypixel_api.py

--- hypixel_api.py
+++ hypixel_api.py
@@ -8,6 +8,7 @@ AUCTIONS_ENDED_URL = "https://api.hypixel.net/v2/skyblock/auctions_ended"
 ACTIVE_AUCTIONS_URL = "https://api.hypixel.net/v2/skyblock/auctions"
 LOWEST_BIN_URL = "https://moulberry.codes/lowestbin.json"
 COFLNET_PRICE_URL = "https://sky.coflnet.com/api/item/price/{item_id}/current"
+COFLNET_HISTORY_URL = "https://sky.coflnet.com/api/item/price/{item_id}/history/day"
 MOJANG_URL = "https://api.mojang.com/users/profiles/minecraft"
 PROFILES_URL = "https://api.hypixel.net/v2/skyblock/profiles"
 ITEMS_URL = "https://api.hypixel.net/resources/skyblock/items"  # no API key needed
